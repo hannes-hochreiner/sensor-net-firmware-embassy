3ac0607bbbd66103b8d98ed07663a0c3e05e9006
Hannes Hochreiner <hannes@hochreiner.net>
2021-10-30 19:50:30 +0200

    add crc check

--- src/common/sht4x.rs
+++ src/common/sht4x.rs
@@ -1,6 +1,5 @@
-use embassy_traits::i2c::I2c;
-// use embedded_hal::blocking::i2c::Write;
 use embassy::time::{Duration, Timer};
+use embassy_traits::i2c::I2c;
 
 pub struct SHT4X<'a, I2C> {
     pub i2c: &'a mut I2C,
@@ -10,6 +9,27 @@ pub enum Sht4xError {
     IdError,
     CrcError,
     TimerError,
+}
+
+impl SHT4X<'_, _> {
+    fn crc8(buffer: &[u8]) -> u8 {
+        let mut rem = 0xff;
+        let poly = 0x31;
+
+        for byte in buffer {
+            rem ^= byte;
+
+            for _i in 1..9 {
+                if rem & 0x80 == 0x80 {
+                    rem = (rem << 1) ^ poly;
+                } else {
+                    rem = rem << 1;
+                }
+            }
+        }
+
+        rem
+    }
 }
 
 impl<I2C> SHT4X<'_, I2C>
@@ -23,8 +43,12 @@ where
         Timer::after(Duration::from_millis(1)).await;
         let res = self.i2c.read(0x44, &mut buffer).await;
 
-        Ok(u32::from_be_bytes([
-            buffer[0], buffer[1], buffer[3], buffer[4],
-        ]))
+        if buffer[2] != crc8(&buffer[0..2]) || buffer[5] != crc8(&buffer[3..5]) {
+            Err(Sht4xError::CrcError)
+        } else {
+            Ok(u32::from_be_bytes([
+                buffer[0], buffer[1], buffer[3], buffer[4],
+            ]))
+        }
     }
 }
