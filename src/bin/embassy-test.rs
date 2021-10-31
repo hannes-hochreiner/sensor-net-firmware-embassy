2dbb736ff4ab59b5f0df8f2e677ad3198a3d2475
Hannes Hochreiner <hannes@hochreiner.net>
2021-10-31 10:03:26 +0100

    add measurements

--- src/bin/embassy-test.rs
+++ src/bin/embassy-test.rs
@@ -51,8 +51,8 @@ async fn main(_spawner: Spawner, mut p: Peripherals) {
         address: 0x44,
     };
 
-    let _serial = sht4.read_serial().await.unwrap();
-    let _measurement = sht4.get_measurement(&sht4x::Precision::High).await.unwrap();
+    let serial = sht4.read_serial().await.unwrap();
+    let measurement = sht4.get_measurement(&sht4x::Precision::High).await.unwrap();
 
     mem::drop(sht4);
     mem::drop(twi);
@@ -61,9 +61,12 @@ async fn main(_spawner: Spawner, mut p: Peripherals) {
     /* RADIO */
     let mut radio_config = radio::Config::default();
 
+    radio_config.mode = radio::Mode::Ble1Mbit;
     radio_config.frequency = radio::Frequency::new(2490).unwrap();
+    radio_config.length_length = radio::Value4Bit::new(8).unwrap();
     radio_config.base_address_0 = 0xABCDABCD;
     radio_config.prefix_0 = 0xEF;
+    radio_config.endianness = radio::Endianness::Big;
     radio_config.crc_length = radio::CrcLength::Crc3bytes;
     radio_config.crc_poly = radio::Value24Bit::new(0b00000000_00000110_01011011).unwrap();
 
@@ -73,13 +76,11 @@ async fn main(_spawner: Spawner, mut p: Peripherals) {
 
     let mut radio_irq = interrupt::take!(RADIO);
     let mut radio = Radio::new(&mut p.RADIO, &mut radio_irq, radio_config);
-    let mut packet: [u8; 29] = [0; 29];
 
     // create package
-    // let mut package: [u8; 28] = [0; 28];
+    let mut packet: [u8; 29] = [0; 29];
     let device_id = 0u64;
     let part_id = 0u32;
-    let serial = 0u32;
     let index = 0u32;
 
     packet[0] = 29;
@@ -88,8 +89,8 @@ async fn main(_spawner: Spawner, mut p: Peripherals) {
     packet[11..15].copy_from_slice(&part_id.to_le_bytes()[..]);
     packet[15..19].copy_from_slice(&index.to_le_bytes()[..]);
     packet[19..21].copy_from_slice(&serial.to_le_bytes()[0..2]);
-    packet[21..25].copy_from_slice(&25f32.to_le_bytes()[..]);
-    packet[25..29].copy_from_slice(&50f32.to_le_bytes()[..]);
+    packet[21..25].copy_from_slice(&measurement.temperature.to_le_bytes()[..]);
+    packet[25..29].copy_from_slice(&measurement.humidity.to_le_bytes()[..]);
 
     radio.transmit(&radio_tx_config, &packet).await.unwrap();
 
