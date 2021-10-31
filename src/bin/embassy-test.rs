d960943e8a9340a4928710204fd55836cc07ad57
Hannes Hochreiner <hannes@hochreiner.net>
2021-10-31 10:30:29 +0100

    add loop

--- src/bin/embassy-test.rs
+++ src/bin/embassy-test.rs
@@ -36,29 +36,117 @@ async fn main(_spawner: Spawner, mut p: Peripherals) {
 
     clock.set_lf_clock_config(&lf_clock_config).await.unwrap();
     /* CLOCK */
-
+    /* IRQs */
+    let mut twim_irq = interrupt::take!(TWIM0_TWIS0_TWI0);
+    let mut radio_irq = interrupt::take!(RADIO);
+    /* IRQs */
     /* TWIM */
-    let mut irq = interrupt::take!(TWIM0_TWIS0_TWI0);
-    let mut config = twim::Config::default();
-
-    config.sda_pullup = true;
-    config.scl_pullup = true;
-    config.frequency = twim::Frequency::K400;
-
-    let mut twi = Twim::new(&mut p.TWI0, &mut irq, &mut p.P0_22, &mut p.P0_23, config);
+    let mut twi = Twim::new(
+        &mut p.TWI0,
+        &mut twim_irq,
+        &mut p.P0_22,
+        &mut p.P0_23,
+        get_twim_config(),
+    );
     let mut sht4 = sht4x::SHT4X {
         i2c: &mut twi,
         address: 0x44,
     };
 
     let serial = sht4.read_serial().await.unwrap();
-    let measurement = sht4.get_measurement(&sht4x::Precision::High).await.unwrap();
 
     mem::drop(sht4);
     mem::drop(twi);
     /* TWIM */
 
-    /* RADIO */
+    let mut index = 0u32;
+
+    loop {
+        /* SAADC */
+        // let config = Config {
+        //     resolution: embassy_nrf::saadc::Resolution::_10BIT,
+        //     gain: embassy_nrf::saadc::Gain::GAIN1,
+        //     oversample: embassy_nrf::saadc::Oversample::OVER16X,
+        //     reference: embassy_nrf::saadc::Reference::INTERNAL,
+        //     resistor: embassy_nrf::saadc::Resistor::BYPASS,
+        //     time: embassy_nrf::saadc::Time::_10US,
+        // };
+        // // let config = Config::default();
+        // let mut saadc = OneShot::new(p.SAADC, interrupt::take!(SAADC), config);
+
+        // loop {
+        //     let sample = saadc.sample(&mut p.P0_03).await;
+        //     let voltage: f32 = sample as f32 * 0.6 / 1024.0 / 0.4;
+        //     let _tmp = 5;
+        //     // info!("sample: {=i16}", sample);
+        //     // Timer::after(Duration::from_millis(100)).await;
+        // }
+        /* SAADC */
+        /* Measurement */
+        let mut twi = Twim::new(
+            &mut p.TWI0,
+            &mut twim_irq,
+            &mut p.P0_22,
+            &mut p.P0_23,
+            get_twim_config(),
+        );
+        let mut sht4 = sht4x::SHT4X {
+            i2c: &mut twi,
+            address: 0x44,
+        };
+
+        let measurement = sht4.get_measurement(&sht4x::Precision::High).await.unwrap();
+
+        mem::drop(sht4);
+        mem::drop(twi);
+        /* Measurement */
+        /* Transmission */
+        let mut radio = Radio::new(&mut p.RADIO, &mut radio_irq, get_radio_config());
+
+        // create package
+        let mut packet: [u8; 29] = [0; 29];
+        let device_id = 0u64;
+        let part_id = 0u32;
+
+        packet[0] = 29;
+        packet[1..3].copy_from_slice(&4u16.to_le_bytes()[..]);
+        packet[3..11].copy_from_slice(&device_id.to_le_bytes()[..]);
+        packet[11..15].copy_from_slice(&part_id.to_le_bytes()[..]);
+        packet[15..19].copy_from_slice(&index.to_le_bytes()[..]);
+        packet[19..21].copy_from_slice(&serial.to_le_bytes()[0..2]);
+        packet[21..25].copy_from_slice(&measurement.temperature.to_le_bytes()[..]);
+        packet[25..29].copy_from_slice(&measurement.humidity.to_le_bytes()[..]);
+
+        radio
+            .transmit(&get_radio_tx_config(), &packet)
+            .await
+            .unwrap();
+        /* Transmission */
+
+        index += 1;
+
+        /* Reception */
+        // let mut rx_packet: [u8; 256] = [0; 256];
+        // let mut radio_rx_config = radio::RxConfig::default();
+
+        // radio_rx_config.rx_address_0_active = true;
+
+        // let _rssi = radio
+        //     .receive(&radio_rx_config, &mut rx_packet)
+        //     .await
+        //     .unwrap();
+
+        // let _packet_length = rx_packet[0];
+        // let _packet_type = u16::from_le_bytes([rx_packet[1], rx_packet[2]]);
+
+        // mem::drop(radio);
+        /* Reception */
+
+        Timer::after(Duration::from_millis(1000)).await;
+    }
+}
+
+fn get_radio_config() -> radio::Config {
     let mut radio_config = radio::Config::default();
 
     radio_config.mode = radio::Mode::Ble1Mbit;
@@ -70,65 +158,23 @@ async fn main(_spawner: Spawner, mut p: Peripherals) {
     radio_config.crc_length = radio::CrcLength::Crc3bytes;
     radio_config.crc_poly = radio::Value24Bit::new(0b00000000_00000110_01011011).unwrap();
 
+    radio_config
+}
+
+fn get_radio_tx_config() -> radio::TxConfig {
     let mut radio_tx_config = radio::TxConfig::default();
 
     radio_tx_config.tx_power = radio::TxPower::Pos4dBm;
 
-    let mut radio_irq = interrupt::take!(RADIO);
-    let mut radio = Radio::new(&mut p.RADIO, &mut radio_irq, radio_config);
+    radio_tx_config
+}
 
-    // create package
-    let mut packet: [u8; 29] = [0; 29];
-    let device_id = 0u64;
-    let part_id = 0u32;
-    let index = 0u32;
+fn get_twim_config() -> twim::Config {
+    let mut config = twim::Config::default();
 
-    packet[0] = 29;
-    packet[1..3].copy_from_slice(&4u16.to_le_bytes()[..]);
-    packet[3..11].copy_from_slice(&device_id.to_le_bytes()[..]);
-    packet[11..15].copy_from_slice(&part_id.to_le_bytes()[..]);
-    packet[15..19].copy_from_slice(&index.to_le_bytes()[..]);
-    packet[19..21].copy_from_slice(&serial.to_le_bytes()[0..2]);
-    packet[21..25].copy_from_slice(&measurement.temperature.to_le_bytes()[..]);
-    packet[25..29].copy_from_slice(&measurement.humidity.to_le_bytes()[..]);
+    config.sda_pullup = true;
+    config.scl_pullup = true;
+    config.frequency = twim::Frequency::K400;
 
-    radio.transmit(&radio_tx_config, &packet).await.unwrap();
-
-    // let mut rx_packet: [u8; 256] = [0; 256];
-    // let mut radio_rx_config = radio::RxConfig::default();
-
-    // radio_rx_config.rx_address_0_active = true;
-
-    // let _rssi = radio
-    //     .receive(&radio_rx_config, &mut rx_packet)
-    //     .await
-    //     .unwrap();
-
-    // let _packet_length = rx_packet[0];
-    // let _packet_type = u16::from_le_bytes([rx_packet[1], rx_packet[2]]);
-
-    // mem::drop(radio);
-    /* RADIO */
-
-    /* SAADC */
-    // let config = Config {
-    //     resolution: embassy_nrf::saadc::Resolution::_10BIT,
-    //     gain: embassy_nrf::saadc::Gain::GAIN1,
-    //     oversample: embassy_nrf::saadc::Oversample::OVER16X,
-    //     reference: embassy_nrf::saadc::Reference::INTERNAL,
-    //     resistor: embassy_nrf::saadc::Resistor::BYPASS,
-    //     time: embassy_nrf::saadc::Time::_10US,
-    // };
-    // // let config = Config::default();
-    // let mut saadc = OneShot::new(p.SAADC, interrupt::take!(SAADC), config);
-
-    // loop {
-    //     let sample = saadc.sample(&mut p.P0_03).await;
-    //     let voltage: f32 = sample as f32 * 0.6 / 1024.0 / 0.4;
-    //     let _tmp = 5;
-    //     // info!("sample: {=i16}", sample);
-    //     // Timer::after(Duration::from_millis(100)).await;
-    // }
-    /* SAADC */
-    Timer::after(Duration::from_millis(1000)).await;
+    config
 }
